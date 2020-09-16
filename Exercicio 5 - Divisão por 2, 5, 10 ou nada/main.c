2c77b7bdd5c70f4fb647c8f237b2f85fe78529f7
guilhermeSaoMarcos <70214455+guilhermeSaoMarcos@users.noreply.github.com>
2020-09-16 12:14:21 -0300

    Add files via upload

---
+++ Exercicio 5 - Divisão por 2, 5, 10 ou nada/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int e_numero;
+
+int main(void) {
+  printf("Digite um valor:\n");
+  scanf("%d", &e_numero);
+
+  if(e_numero % 2 == 0){
+    printf("O numero %d é divisivel por 2\n", e_numero);
+    if(e_numero % 5 == 0){
+      printf("O numero %d é divisivel por 5\n", e_numero);
+      if(e_numero % 5 == 0){
+        printf("O numero %d é divisivel por 10\n", e_numero);
+      }
+    }
+  }else{
+    if(e_numero % 5 == 0){
+      printf("O numero %d é divisivel por 5\n", e_numero);
+      if(e_numero % 10 == 0){
+        printf("O numero %d é divisivel por 10\n", e_numero);
+      }
+    }else{
+      if(e_numero % 10 == 0){
+        printf("o numero %d é divisivel por 2\n", e_numero);
+      }else{
+        if(e_numero % 2 != 0){
+          if(e_numero % 5 != 0){
+            if(e_numero % 10 != 0){
+              printf("O numero %d NÃO é divisivel por 2, 5 e 10\n", e_numero);
+            }
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
